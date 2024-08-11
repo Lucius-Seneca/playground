8e0c6480ef15816648865c314625962dd5984713
Mathias Schickel <mathias.schickel@gmail.com>
2024-08-11 20:58:23 +0200

    Improve format in app.

--- app/app.cpp
+++ app/app.cpp
@@ -3,12 +3,19 @@
 #include <iostream>
 
 int main() {
-  Node node_successor_1(2);
-  const Node::Nodes successor{node_successor_1};
-  Node node_successor_2(3, std::make_shared<Node::Nodes>(successor));
-  const Node::Nodes successors{node_successor_1, node_successor_2};
+  // Generate a graph of structure
+  // Node 1              1           n → m : n has child m
+  // ├─ Node 2 ─┐      ↙   ↘
+  // └─ Node 3  │    2   ←   3
+  //    └───────┘
+  Node node_2(2);
+  const Node::Nodes children_of_node_3{node_2};
+
+  Node node_3(3, std::make_shared<Node::Nodes>(children_of_node_3));
+  const Node::Nodes children_of_node_1{node_2, node_3};
+
   Node node(
-      1, std::make_shared<Node::Nodes>(successors));
+      1, std::make_shared<Node::Nodes>(children_of_node_1));
   std::cout << node << std::endl;
 
   return EXIT_SUCCESS;
